8dd2c39e3a8d2cbed38e3b54b84c3cb6b1924488
Amethesh LGP <ametheshlgp@gmail.com>
2023-05-26 23:04:24 +0530

    styling navbar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,11 +1,17 @@
 import Link from "next/link";
+import style from "./navbar.module.css";
+import Image from "next/image";
+import logo from "../../assets/logo.png";
 
 export default function Navbar() {
   return (
-    <div>
-      <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
-      <Link href="/waifu">Waifu Generator</Link>
+    <div className={style.container}>
+      <nav className={style.navbar}>
+        <Image src={logo} alt="Logo" width={200} height={50} />
+        <Link href="/">Home</Link>
+        <Link href="/waifu">Waifu Generator</Link>
+        <Link href="/about">About</Link>
+      </nav>
     </div>
   );
 }
