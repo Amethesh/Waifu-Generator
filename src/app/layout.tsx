9a6d196d93ecd85f2f2e4a1a22ba71594d9bd553
Amethesh LGP <ametheshlgp@gmail.com>
2023-05-27 21:07:18 +0530

    Styling button and checkbox

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,24 @@
 import Navbar from "./components/navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Waifu Generator",
-  description: "A website to generate random Anime Waifu, Wallpapers and more",
+	title: "Waifu Generator",
+	description: "A website to generate random Anime Waifu, Wallpapers and more"
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Navbar />
-        {children}
-      </body>
-    </html>
-  );
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<html lang="en">
+			<body className={inter.className}>
+				<Navbar />
+				{children}
+			</body>
+		</html>
+	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import Head from "next/head";
 import Image from "next/image";
 import { useState } from "react";
 import getWaifu from "./getWaifu";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDownload, faLink } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
 	const [category, setCategory] = useState<string>("waifu");
@@ -25,8 +27,8 @@ export default function Home() {
 		}
 	};
 
-	const handleCategoryChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-		setCategory(event.target.value);
+	const handleCategoryChange = (value: string) => {
+		setCategory(value);
 	};
 
 	const handleTypeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
@@ -40,21 +42,108 @@ export default function Home() {
 			</Head>
 			<section className={styles.container}>
 				<div className={styles.leftsection}>
-					<main className={styles.text}>Select type or style of the image you need to generate</main>
-					<form className={styles.form}>
-						<select value={category} onChange={handleCategoryChange}>
-							<option>waifu</option>
-							<option>neko</option>
-							<option>shinobu</option>
-							<option>happy</option>
-						</select>
-
-						<select value={type} onChange={handleTypeChange}>
-							<option>sfw</option>
-							<option>nsfw</option>
-						</select>
-					</form>
-					<button onClick={generateWaifu}>Generate</button>
+					<main className={styles.text}>Select your Type</main>
+					<div className="toggle">
+						<input
+							id="toggle-input"
+							type="checkbox"
+							checked={type === "nsfw"}
+							onChange={() => setType(type === "nsfw" ? "sfw" : "nsfw")}
+						/>
+						<label htmlFor="toggle-input"></label>
+					</div>
+					{type === "sfw" && (
+						<form className="check-form">
+							<div>
+								<input
+									type="checkbox"
+									id="waifu"
+									checked={category.includes("waifu")}
+									onChange={() => handleCategoryChange("waifu")}
+								/>
+								<label htmlFor="waifu">Waifu</label>
+							</div>
+							<div>
+								<input
+									type="checkbox"
+									id="neko"
+									checked={category.includes("neko")}
+									onChange={() => handleCategoryChange("neko")}
+								/>
+								<label htmlFor="neko">Neko</label>
+							</div>
+							<div>
+								<input
+									type="checkbox"
+									id="shinobu"
+									checked={category.includes("shinobu")}
+									onChange={() => handleCategoryChange("shinobu")}
+								/>
+								<label htmlFor="shinobu">Shinobu</label>
+							</div>
+							<div>
+								<input
+									type="checkbox"
+									id="megumin"
+									checked={category.includes("megumin")}
+									onChange={() => handleCategoryChange("megumin")}
+								/>
+								<label htmlFor="megumin">Megumin</label>
+							</div>
+							<div>
+								<input
+									type="checkbox"
+									id="bully"
+									checked={category.includes("bully")}
+									onChange={() => handleCategoryChange("bully")}
+								/>
+								<label htmlFor="bully">Bully</label>
+							</div>
+							<div>
+								<input
+									type="checkbox"
+									id="cuddle"
+									checked={category.includes("cuddle")}
+									onChange={() => handleCategoryChange("cuddle")}
+								/>
+								<label htmlFor="cuddle">Cuddle</label>
+							</div>
+						</form>
+					)}
+					{type === "nsfw" && (
+						<form className="check-form">
+							<div>
+								<input
+									type="checkbox"
+									id="waifu"
+									checked={category.includes("waifu")}
+									onChange={() => handleCategoryChange("waifu")}
+								/>
+								<label htmlFor="waifu">Waifu</label>
+							</div>
+							<div>
+								<input
+									type="checkbox"
+									id="neko"
+									checked={category.includes("neko")}
+									onChange={() => handleCategoryChange("neko")}
+								/>
+								<label htmlFor="neko">Neko</label>
+							</div>
+							<div>
+								<input
+									type="checkbox"
+									id="trap"
+									checked={category.includes("trap")}
+									onChange={() => handleCategoryChange("trap")}
+								/>
+								<label htmlFor="trap">Trap</label>
+							</div>
+						</form>
+					)}
+					<button className={styles.generate} onClick={generateWaifu}>
+						Generate
+					</button>
 				</div>
 				<div className={styles.rightsection}>
 					{isLoading ? (
@@ -63,8 +152,14 @@ export default function Home() {
 						<img src={waifuImage} alt="Random waifu image" />
 					)}
 					<div className={styles.buttons}>
-						<button>Download</button>
-						<button>Copy Link</button>
+						<button>
+							<FontAwesomeIcon icon={faDownload} style={{ marginRight: "5px" }} />
+							Download
+						</button>
+						<button>
+							<FontAwesomeIcon icon={faLink} style={{ marginRight: "5px" }} />
+							Copy Link
+						</button>
 					</div>
 				</div>
 			</section>
