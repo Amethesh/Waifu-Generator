004d63bfd450dec590497bfef1fbbe016a07bc2e
Amethesh LGP <ametheshlgp@gmail.com>
2023-05-27 13:48:58 +0530

    Finishing style

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -7,12 +7,15 @@ export default function Navbar() {
 	return (
 		<div className={style.container}>
 			<nav className={style.navbar}>
-				<div className={style.imagewrapper}>
-					<Image src={logo} alt="Logo" width={200} />
-				</div>
-				<Link href="/">Home</Link>
-				<Link href="/waifu">Waifu Generator</Link>
-				<Link href="/about">About</Link>
+				<Link id="links" href="/">
+					Home
+				</Link>
+				{/* <div className={style.imagewrapper}> */}
+				<Image src={logo} alt="Logo" width={150} />
+				{/* </div> */}
+				<Link id="links" href="/about">
+					About
+				</Link>
 			</nav>
 		</div>
 	);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,70 +6,57 @@ import { useState } from "react";
 import getWaifu from "./getWaifu";
 
 export default function Home() {
-  const [category, setCategory] = useState<string>("waifu");
-  const [type, setType] = useState<string>("sfw");
-  const [waifuImage, setWaifuImage] = useState<string>(
-    "https://i.waifu.pics/3DpVCc3.jpg"
-  );
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+	const [category, setCategory] = useState<string>("waifu");
+	const [type, setType] = useState<string>("sfw");
+	const [waifuImage, setWaifuImage] = useState<string>("https://i.waifu.pics/3DpVCc3.jpg");
+	const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  const generateWaifu = async () => {
-    setIsLoading(true);
-    const data = await getWaifu({ type, category });
-    setWaifuImage(data.url);
-    setIsLoading(false);
-  };
+	const generateWaifu = async () => {
+		setIsLoading(true);
+		const data = await getWaifu({ type, category });
+		setWaifuImage(data.url);
+		setIsLoading(false);
+	};
 
-  const handleCategoryChange = (
-    event: React.ChangeEvent<HTMLSelectElement>
-  ) => {
-    setCategory(event.target.value);
-  };
+	const handleCategoryChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+		setCategory(event.target.value);
+	};
 
-  const handleTypeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setType(event.target.value);
-  };
+	const handleTypeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+		setType(event.target.value);
+	};
 
-  return (
-    <>
-      <Head>
-        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-      </Head>
-      <main className={styles.main}>This is the Home Page</main>
-      <section>
-        <form>
-          <select value={category} onChange={handleCategoryChange}>
-            <option>waifu</option>
-            <option>neko</option>
-            <option>shinobu</option>
-            <option>happy</option>
-          </select>
+	return (
+		<>
+			<Head>
+				<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+			</Head>
+			<section className={styles.container}>
+				<div className={styles.leftsection}>
+					<main className={styles.text}>Select type or style of the image you need to generate</main>
+					<form className={styles.form}>
+						<select value={category} onChange={handleCategoryChange}>
+							<option>waifu</option>
+							<option>neko</option>
+							<option>shinobu</option>
+							<option>happy</option>
+						</select>
 
-          <select value={type} onChange={handleTypeChange}>
-            <option>sfw</option>
-            <option>nsfw</option>
-          </select>
-        </form>
-        <button onClick={generateWaifu}>Generate</button>
-        <div>
-          {isLoading ? (
-            <div
-              style={{
-                width: "300px",
-                height: "500px",
-                backgroundColor: "white",
-              }}
-            ></div>
-          ) : (
-            <Image
-              src={waifuImage}
-              alt="Random waifu image"
-              width={300}
-              height={500}
-            />
-          )}
-        </div>
-      </section>
-    </>
-  );
+						<select value={type} onChange={handleTypeChange}>
+							<option>sfw</option>
+							<option>nsfw</option>
+						</select>
+					</form>
+					<button onClick={generateWaifu}>Generate</button>
+				</div>
+				<div className={styles.rightsection}>
+					{isLoading ? <div className={styles.loading}></div> : <img src={waifuImage} alt="Random waifu image" />}
+					<div className={styles.buttons}>
+						<button>Download</button>
+						<button>Copy Link</button>
+					</div>
+				</div>
+			</section>
+		</>
+	);
 }
